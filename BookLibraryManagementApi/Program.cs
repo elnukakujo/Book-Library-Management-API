d7044ba1a3209945988d1cdfd3b180b9dc03244f
elnukakujo <jagernoe1s3@hotmail.com>
2024-06-28 12:42:13 -0400

    Added middleware

--- BookLibraryManagementApi/Program.cs
+++ BookLibraryManagementApi/Program.cs
@@ -1,5 +1,15 @@
+using Microsoft.AspNetCore.Rewrite;
+
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
+
+app.UseRewriter(new RewriteOptions().AddRedirect("book/(.*)", "books/$1"));
+app.Use(async (context, next) =>
+{
+    Console.WriteLine($"[{context.Request.Method} {context.Request.Path} {DateTime.UtcNow}] Started.");
+    await next(context);
+    Console.WriteLine($"[{context.Request.Method} {context.Request.Path} {DateTime.UtcNow}] Finished.");
+});
 
 var books = new List<Book>();
 app.MapGet("/books", () => books);
